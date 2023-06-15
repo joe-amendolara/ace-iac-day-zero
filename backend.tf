e65796ce544463d84545ce8dfd16d37dddf63505
joe-amendolara <117191952+joe-amendolara@users.noreply.github.com>
2023-06-15 17:17:37 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.2.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "Aviatrix-Joe"
+    organization = "Aviatrix-JoeAmendolara"
     workspaces {
       name = "ace-iac-day-zero"
     }
